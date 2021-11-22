437af201b36263e39714baf207c08b93f8b5c949
Clément Saussaye <94860890+fheern@users.noreply.github.com>
2021-11-22 19:09:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+console.log("ready!");
+
+$("#works a").mouseenter(function(){
+	console.log("enter planete");
+	$(".planete").css({"position":"absolute"})
+	.animate({"width":"+=5vw"},200)
+	$(".ceinture").animate({"width":"+=7vw"},200)
+	$(".rocket").css({"position":"absolute"})
+	.animate({"width":"+=28vw"},200);
+});
+
+$("#works a").mouseleave(function(){
+	console.log("leave planete");
+	$(".planete").animate({"width":"-=5vw"},200)
+	$(".ceinture").animate({"width":"-=7vw"},200)
+		$(".rocket").css({"position":"absolute"})
+	.animate({"width":"-=28vw"},200)
+	
+});
+
+$("#head a").click(function(){
+	$('html, body').animate({
+		scrollTop: $($.attr(this,'href')).offset().top
+	}, 600);
+});
+
+});
+
+
+// $("#works a").mouseenter(function(){
+// 	console.log("enter planete");
+
+// 	$(".planete").css({"position":"absolute"})
+// 	.animate({"width":"+=5vw"},200);
+
+// 	$(".ceinture").animate({"width":"+=5vw"},200);
+
+// 	$(".rocket").css({"display":"block"})
+// 	.animate({opacity: "+=0,5",left:"+=10vw",width:"+=10vw"},200);
+// });
+
+// $("#works a").mouseleave(function(){
+// 	console.log("leave planete");
+
+// 	$(".planete").animate({width:"-=5vw"},200);
+
+// 	$(".ceinture").animate({width:"-=5vw"},200);
+
+// 	$(".rocket").animate({opacity:"-=1",left:"+=80vw",width:"10px"},200);
+
+// 	setTimeout(function(){
+// 	$(".rocket").animate({"width": "10vw","left": "0px"});
+// 	console.log("rocket replacée");
+// 	}, 210);
+// });
+
+
+
+
