8605ca840af88e3d3a4be35629df22afb8428714
Clément Saussaye <94860890+fheern@users.noreply.github.com>
2021-11-26 02:26:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -20,12 +20,6 @@ $("#works a").mouseleave(function(){
 	
 });
 
-$("#head a").click(function(){
-	$('html, body').animate({
-		scrollTop: $($.attr(this,'href')).offset().top
-	}, 600);
-});
-
 });
 
 
